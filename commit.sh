4ed7178d3d5582542fa36224315d3748e695ad6c
Sebas <sebastian.v.a@riseup.net>
2024-07-29 12:45:15 +0200

    sync from git to azure

--- commit.sh
+++ commit.sh
@@ -1,24 +1,32 @@
+#!/bin/bash
+
+# Set environment variables
 AZUREPAT=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
+
+# Clone the GitHub repository
 git clone https://github.com/dadapunk/js-e2e-express-server
 cd js-e2e-express-server
 rm -rf .git
-
 cd ..
 
+# Clone the Azure repository
 GIT_CMD_REPOSITORY="https://irep-tech@dev.azure.com/irep-tech/Github%20Integration%20test/_git/Github%20Integration%20test"
 git clone $GIT_CMD_REPOSITORY
 
-cp -r js-e2e-express-server/* js-e2e-express-server/
+# Copy files from GitHub repository to Azure repository
+cp -r js-e2e-express-server/* Github\ Integration\ test/
 
-cd js-e2e-express-server
+# Change to the Azure repository directory
+cd Github\ Integration\ test
 
+# Configure Git user
 git config --global user.email "$AZUSER_EMAIL"
 git config --global user.name "$AZUSERNAME"
 
+# Add, commit, and push changes
 git add .
 git commit -m "sync from git to azure"
-
 git push
